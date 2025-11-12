0a716f2fa94664f3e5467d5ed327bc227e7229ac
Dante Hernandez <dantehernandez587@gmail.com>
2025-11-12 09:50:49 -0500

    okay

---
+++ react_v1/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from '../src//components/card.jsx'
+
+function App() {
+  // I am declaring a variable x and setting it to 0
+  
+
+  return (
+    <>
+    <h1> This is my first day with react </h1>
+    <p> Dante Hernandez </p>
+
+    <p> Dante Hernandez </p>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+
+    </>
+  )
+}
+
+export default App
---
+++ react_v1/src/components/card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {useState} from 'react'
+import './card.css'
+
+function Card() {
+const [x,setx]= useState(0);
+function add(){
+setx(x+1);
+}
+  return (
+    <div class="card">
+      <p>This is a card</p>
+      {/* Note-1: in order to use javascript inside jsx, we must enclose everything inside curly braces */ }
+      <p> {x} </p>
+      {/* Note-2: Instead of using getElementById and adding an event listener, I can directly use onClick to trigger   */}
+      {/* Note-3: When calling a function inside jsx, you don't put parenthisis() */}
+      <button onClick={add}>Increase number</button>
+    </div>
+  )
+}
+
+export default Card
